632e32b91ae9a62e5e7d86e6f8998e359a0aafb3
Chinmay Chaudhari <111864031+BrawlerXull@users.noreply.github.com>
2024-10-22 09:59:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ calculatorForm.addEventListener('submit', (e) => {
     const num2Value = parseFloat(num2.value);
     const selectedOperator = operator.value;
 
-    fetch('http://35.154.20.70:5000/calculate', {
+    fetch('http://3.110.124.171:5000/calculate', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
